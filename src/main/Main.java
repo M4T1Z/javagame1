bdb9a67176793695d57d9b80eb3de9481f78b5cd
M4T1Z <matias01.2001@gmail.com>
2024-01-07 13:57:51 +0100

    V0.002

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,11 @@
 package main;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args){
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         JFrame okno = new JFrame();
         okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         okno.setResizable(false);
@@ -14,6 +16,7 @@ public class Main {
 
         Panel panel = new Panel();
         okno.add(panel);
+        okno.setLocation((screenSize.width - panel.szerokosc)/2,(screenSize.height - panel.wysokosc)/2);
         okno.pack();
 
         panel.setupObject();
